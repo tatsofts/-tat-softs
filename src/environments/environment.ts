5e37b14b4a1e48c1ead85e4c828d007f841c6aeb
vamseeuxd <vamsee.uxd@gmail.com>
2022-04-10 09:18:48 +0530

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'my-monthly-money-manager',
+    appId: '1:879163289391:web:4a096a2672e25daf20fbda',
+    storageBucket: 'my-monthly-money-manager.appspot.com',
+    apiKey: 'AIzaSyD8xcA3Z8Ciqk9JseJlU3IzSQNitKGqqm4',
+    authDomain: 'my-monthly-money-manager.firebaseapp.com',
+    messagingSenderId: '879163289391',
+    measurementId: 'G-L7Y6Q46SPQ',
+  },
   production: false
 };
 
