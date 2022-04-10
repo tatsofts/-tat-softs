0a62871152f1bb0186d4ce0a3e5962441b64e252
vamseeuxd <vamsee.uxd@gmail.com>
2022-04-10 09:13:25 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tat-softs-ui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
